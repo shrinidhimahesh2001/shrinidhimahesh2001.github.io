eaa18cd6da3f696a4acd4f2651c3375b46cc528c
Shrinidhi Mahesh <66459886+shrinidhimahesh2001@users.noreply.github.com>
2026-03-01 18:47:20 -0800

    Updated Descriptions and Added Blog+Cover Account URL

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -2,7 +2,7 @@ export interface AboutMe {
   name: string;
   title: string;
   institution: string;
-  description: string;
+  description: string | string[];
   email: string;
   imageUrl?: string;
   soundcloudUrl?: string;
@@ -11,6 +11,7 @@ export interface AboutMe {
   twitterUsername?: string;
   githubUsername?: string;
   linkedinUsername?: string;
+  blogUrl?: string;
   funDescription?: string; // Gets placed in the left sidebar
   secretDescription?: string; // Gets placed in the bottom
   altName?: string;
@@ -22,18 +23,22 @@ export const aboutMe: AboutMe = {
   title: "Masters of Science - Electrical Engineering (Machine Learning and Data Science)",
   institution: "University of Southern California",
   // Note that links work in the description
-  description:
-    "Hi there,My name is Shrinidhi Mahesh and I am wrapping up a MS in EE from USC with my courses focused towards MLDS .My technical background combines Cloud Data Engineering production experience at Deloitte along with rigorous graduate coursework at USC in Deep Learning, Machine Learning, Reinforcement Learning and LLMs and Speech Processing. I have also been passionately training in Carnatic Music as a vocalist for the past 10+ years which drives my interest for Music Information Retrieval (MIR) and Audio ML.",
+  description:[
+    "Hi there,My name is Shrinidhi Mahesh and I am wrapping up a MS in EE from USC with my courses focused towards MLDS.",
+    "My technical background combines Cloud Data Engineering production experience at Deloitte along with rigorous graduate coursework at USC in Deep Learning, Machine Learning, Reinforcement Learning and LLMs and Speech Processing.",
+    "I have also been passionately training in Carnatic Music as a vocalist for the past 10+ years which drives my interest for Music Information Retrieval (MIR) and Audio ML."
+  ],
   email: "shrinidhimdev@gmail.com",
   imageUrl:
     "https://avatars.githubusercontent.com/u/66459886?s=400&u=bb66939efe343630e2a09da26f1a1730c45d4eca&v=4",
   googleScholarUrl: "https://scholar.google.com/citations?hl=en&user=Ykjb4AMAAAAJ",
   githubUsername: "shrinidhimahesh2001",
   linkedinUsername: "shrinidhi-mahesh-7680b41a5",
+  blogUrl:"https://shrinidhimahesh.substack.com/",
   twitterUsername: "shrinimahesh",
   soundcloudUrl: "https://soundcloud.com/shrinidhimahesh",
   cvUrl: "https://drive.google.com/file/d/1yh3GFziJ23RSjsmlZDDEydyyNAO6E7cS/view?usp=sharing",
   institutionUrl: "https://usc.edu/",
   // altName: "",
-  // secretDescription: "I like dogs.",
+  secretDescription: "I know 4+ languages and love cooking! I am a huge foodie and slowly picking up chess too!",
 };
--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -1,6 +1,6 @@
 export interface Portfolio {
   title: string;
-  description: string;
+  description: string|string[];
   technologies?: string[];
   imageUrl?: string;
   projectUrl?: string;
@@ -11,8 +11,10 @@ export const portfolioData: Portfolio[] = [
   // Example entry
   {
     title: "Emotion Recognition on MTG-Jamendo Dataset",
-    description:
-      "A short experiment on classifying emotions in music using the MTG-Jamendo dataset. I trained a simple CNN model on the mel-spectrograms of the audio clips and achieved an accuracy of around 70%. The project is still a work in progress and I plan to explore more advanced architectures and techniques to improve the performance.",
+    description:[
+      "A short experiment on classifying emotions in music using the MTG-Jamendo dataset.",
+       "I trained a simple CNN model on the mel-spectrograms of the audio clips and achieved an accuracy of around 70%.",
+       "The project is still a work in progress and I plan to explore more advanced architectures and techniques to improve the performance."],
     technologies: ["Python", "PyTorch", "Librosa", "CNNs"],
     projectUrl: "",
     imageUrl:
@@ -20,9 +22,19 @@ export const portfolioData: Portfolio[] = [
     codeUrl: "https://github.com/shrinidhimahesh2001/EmoteTune",
   },
   {title: "Collection of Carnatic Music Performances and recordings",
-    description: "Over the years, I have been actively involved in performing Carnatic music. This collection includes audio recordings of practice sessions of traditional compositions.",
+    description: [
+      "Over the years, I have been actively involved in performing Carnatic music.",
+      "This collection includes audio recordings of practice sessions of traditional compositions."],
     technologies: [],
     imageUrl: "",
     projectUrl: "https://on.soundcloud.com/hX4A131WN3CpRZv3IO",
-    codeUrl: ""}
+    codeUrl: ""},
+  {title: "Collection of cover songs",
+    description: "A lot of random musings and bathroom singing sessions recorded over the years. This collection includes a mix of popular songs in various Indian languages.",
+    technologies: [],
+    imageUrl: "",
+    projectUrl: "https://www.instagram.com/shrinidhimaheshmusic/",
+    codeUrl: ""
+  }
+  
 ];
